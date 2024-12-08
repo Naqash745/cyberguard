38bff580bb1b077ef6bccb994eb80a11a84bae31
Naqash745 <nakash_92@ymail.com>
2024-12-08 20:27:39 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -287,25 +287,12 @@ if selection=="Education Portal":
         st.caption("What is Cybercrime?")
         st.video("https://youtu.be/X7kFAy1E8Jw?si=lKWx-y3Tz1_dsSQP")
     with col2:
+        st.caption("What is Cybercrime?")
         st.video("https://youtu.be/Yr0xPVFcf-U?si=xNHedIZgSQbUc9f_")
 
-    # col1,col2=st.columns(2)
-    # with col1:
-    #     st.markdown('''
-    #     <iframe width="420" height="315"
-    #     src="https://youtu.be/Yr0xPVFcf-U?si=xNHedIZgSQbUc9f_">
-    #     </iframe>
-    #     ''',
-    #     unsafe_allow_html=True
-    #     )
-    # with col2:
-    #     st.markdown('''
-    #     <iframe width="420" height="315"
-    #     src="https://youtu.be/VJFaO2-zsCU?si=tiKXA6NRT-YCPNJr">
-    #     </iframe>
-    #     ''',
-    #     unsafe_allow_html=True
-    #     )
+    col1,col2=st.columns(2)
+    with col1:
+        st.video("https://youtu.be/VJFaO2-zsCU?si=tiKXA6NRT-YCPNJr")
     # col1,col2=st.columns(2)
     # with col1:
     #     st.markdown('''
