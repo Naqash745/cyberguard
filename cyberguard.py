5936529421f15fb3248b3a49500c238da85a9dc8
Naqash745 <nakash_92@ymail.com>
2024-12-09 15:13:59 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -17,6 +17,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 # from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import sentence_transformers
