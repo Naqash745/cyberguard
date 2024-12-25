bf92370c675ae32179899ba5aa261e8da547f32d
Naqash745 <nakash_92@ymail.com>
2024-12-25 17:57:00 +0500

    Update cyberguard.py

--- cyberguard.py
+++ cyberguard.py
@@ -36,21 +36,21 @@ from langchain_core.output_parsers import StrOutputParser
 
 st.title("CYBER-GUARD")
 
-loader=PyPDFDirectoryLoader("./knowledgebase_for_chatbot/")
-data = loader.load()
-# #split the extracted data into text chunks using the text_splitter, which splits the text based on the specified number of characters and overlap
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-text_chunks = text_splitter.split_documents(data)
-# #download the embeddings to use to represent text chunks in a vector space, using the pre-trained model "sentence-transformers/all-MiniLM-L6-v2"
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-# # # # create embeddings for each text chunk using the FAISS class, which creates a vector index using FAISS and allows efficient searches between vectors
-vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
+# loader=PyPDFDirectoryLoader("./knowledgebase_for_chatbot/")
+# data = loader.load()
+# # #split the extracted data into text chunks using the text_splitter, which splits the text based on the specified number of characters and overlap
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+# text_chunks = text_splitter.split_documents(data)
+# # #download the embeddings to use to represent text chunks in a vector space, using the pre-trained model "sentence-transformers/all-MiniLM-L6-v2"
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# # # # # create embeddings for each text chunk using the FAISS class, which creates a vector index using FAISS and allows efficient searches between vectors
+# vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
 
-vector_store.save_local("faiss_index")
+# vector_store.save_local("faiss_index")
 
-new_vector_store = FAISS.load_local(
-    "faiss_index", embeddings, allow_dangerous_deserialization=True
-)
+# new_vector_store = FAISS.load_local(
+#     "faiss_index", embeddings, allow_dangerous_deserialization=True
+# )
 
 
 
@@ -261,9 +261,9 @@ if selection=="Dashboard":
 
 if selection=="Cyber Awareness Chatbot":
     st.subheader("Cyber Awareness Chatbot")
-    query=st.text_input("Write Query Here")
-    if st.button("Submit"):
-        st.write(rag_chain.invoke(query))
+    # query=st.text_input("Write Query Here")
+    # if st.button("Submit"):
+    #     st.write(rag_chain.invoke(query))
 # if selection=="Malicious File Scanner":
 #     st.subheader("Malicious File Scanner")
 #     file=st.file_uploader("Select a File")
